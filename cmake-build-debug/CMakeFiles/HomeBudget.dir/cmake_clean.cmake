b4d902aa1b9e99237f3691af2c0d70054a19235c
Mateusz A <87kelthuzad@gmail.com>
2019-01-08 22:24:43 +0100

    start

---
+++ cmake-build-debug/CMakeFiles/HomeBudget.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HomeBudget.dir/main.cpp.o"
+  "HomeBudget.pdb"
+  "HomeBudget"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HomeBudget.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
