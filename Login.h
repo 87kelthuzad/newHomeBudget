2249de91f722d7c1b943b9b939233fdb4d147123
Mateusz A <87kelthuzad@gmail.com>
2019-01-12 15:45:45 +0100

    Add Login.cpp, Login.h, login.csv I added methods and variables to Login.h

---
+++ Login.h
@@ -0,0 +1,37 @@
+//
+// Created by mateusz on 12.01.19.
+//
+
+#ifndef HOMEBUDGET_LOGIN_H
+#define HOMEBUDGET_LOGIN_H
+
+#include <string>
+
+using namespace std;
+
+struct recordLogin {
+    string nickCSV;
+    string passwordCSV;
+};
+
+class Login {
+
+private:
+
+    string nick;
+    string password;
+
+    void comparisonOfLoginWithCVS();
+
+public:
+
+    Login();
+    ~Login();
+
+    void setNick( string nick ) { this->nick = nick; }
+    void setPassword( string password ) { this->password = password; }
+
+};
+
+
+#endif //HOMEBUDGET_LOGIN_H
